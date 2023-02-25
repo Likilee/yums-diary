0031d8960eff2468cf972da41a86fd2e4ffa34d9
Likilee <gnsdlrl@daum.net>
2023-02-25 22:07:13 +0900

    fix: layout for pwa

--- src/components/BottomNavigation/index.tsx
+++ src/components/BottomNavigation/index.tsx
@@ -28,7 +28,7 @@ export default function BottomNavigation() {
 
   return (
     <>
-      <div className="btm-nav btm-nav-sm static mb-safebottom">
+      <div className="btm-nav btm-nav-sm static">
         <BottomNavItem href="/">
           <TbCalendar className="text-2xl" />
         </BottomNavItem>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" data-theme="cupcake">
       <Head>
-        <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
+        <meta name="viewport" content="initial-scale=1, viewport-fit=cover, user-scalable=no" />
         <link rel="manifest" href="/pwa/manifest.json" />
         <link href="/pwa/favicon.ico" rel="icon" type="image/x-icon" sizes="16x16" />
         <link rel="apple-touch-icon" href="/pwa/icons/icon-192x192.png" />
