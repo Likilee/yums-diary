0ec226325df4311f431d1545a133634cfa404fb9
Likilee <gnsdlrl@daum.net>
2023-02-25 21:53:24 +0900

    fix: initial-scale

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" data-theme="coffee">
+    <Html lang="en" data-theme="cupcake">
       <Head>
+        <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
         <link rel="manifest" href="/pwa/manifest.json" />
         <link href="/pwa/favicon.ico" rel="icon" type="image/x-icon" sizes="16x16" />
         <link rel="apple-touch-icon" href="/pwa/icons/icon-192x192.png" />
