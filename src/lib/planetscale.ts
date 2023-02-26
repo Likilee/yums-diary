1ec6635e0181baf3de2809d07237103c2b37190a
Likilee <gnsdlrl@daum.net>
2023-02-27 02:05:34 +0900

    refactor: DB 스키마 주석 제거
    DB 스키마의 변경이 아직 빈번하기 때문에, 추가적인 주석 관리 비용만 발생하므로 제거

--- src/lib/planetscale.ts
+++ src/lib/planetscale.ts
@@ -2,17 +2,6 @@
 import { Generated, Kysely } from 'kysely'
 import { PlanetScaleDialect } from 'kysely-planetscale'
 
-/* 💡
-CREATE TABLE daily_notes (
-  id INT NOT NULL AUTO_INCREMENT,
-  date DATE NOT NULL,
-  content TEXT,
-  position INT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
-);
- */
 export interface DailyNote {
   id: Generated<number>
   date: Date
@@ -36,16 +25,6 @@ export interface UpdateDailyNoteDTO {
   content?: string
 }
 
-/* 💡
-CREATE TABLE categories (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
-);
-*/
-
 export interface Category {
   id: Generated<number>
   name: string
@@ -53,19 +32,6 @@ export interface Category {
   updated_at: Generated<Date>
 }
 
-/* 💡
-CREATE TABLE category_notes (
-  id INT NOT NULL AUTO_INCREMENT,
-  category_id INT NOT NULL,
-  date DATE NOT NULL,
-  title VARCHAR(255),
-  content TEXT,
-  position INT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
-);
-*/
 export interface CategoryNote {
   id: Generated<number>
   category_id: number
