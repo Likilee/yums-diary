4448d4106d7fc31e26a3cc8ad47d34beff66742d
Likilee <gnsdlrl@daum.net>
2023-02-26 20:41:57 +0900

    feat: 유미 노트 페이지 공사중 표시

--- src/pages/note.tsx
+++ src/pages/note.tsx
@@ -6,7 +6,7 @@ export default function NotePage() {
   return (
     <main>
       <textarea
-        placeholder="Bio"
+        placeholder="유미의 노트 기능은 조금 기다려 주세요 :)"
         className="textarea textarea-bordered textarea-lg w-full resize-none"
       ></textarea>
     </main>
