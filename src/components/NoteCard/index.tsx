594e68b26cf12d9c067874c09282648be8db428c
Likilee <gnsdlrl@daum.net>
2023-02-27 20:21:48 +0900

    refactor: NoteCard 컴포넌트 내부 pre 태그 textarea 로 변경
    - 바로 수정이 가능한 기능을 추가하기 위해 필요한 작업

--- src/components/NoteCard/index.tsx
+++ src/components/NoteCard/index.tsx
@@ -1,11 +1,20 @@
+import AutoGrowTextarea from '@/components/AutoGrowTextarea'
+
 interface NoteCardProps {
   content: string | null
 }
 
 export default function NoteCard({ content }: NoteCardProps) {
+  const handleTextChange = () => {
+    console.log('temp')
+  }
+
   return (
-    <div className="w-full border border-current rounded-md p-4 text-base">
-      <pre className="whitespace-pre-wrap break-words font-sans">{content}</pre>
-    </div>
+    <AutoGrowTextarea
+      value={content || ''}
+      onChange={handleTextChange}
+      minHeight="1rem"
+      className="textarea textarea-ghost w-full whitespace-pre-wrap break-words resize-none text-lg border border-current rounded-md h-fit"
+    />
   )
 }
