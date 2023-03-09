8013a6021378f9ccebac7f264d99cd5f235547b7
Likilee <gnsdlrl@daum.net>
2023-03-09 23:07:30 +0900

    feat: + 버튼 diary 페이지와 note 페이지에서 다르게 동작하도록 수정

--- src/components/BottomNavigation/index.tsx
+++ src/components/BottomNavigation/index.tsx
@@ -3,6 +3,7 @@ import { isSameFirstPath } from '@/lib/path'
 import classNames from 'classnames'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/router'
 import { PropsWithChildren } from 'react'
 import { TbCalendar, TbNotebook, TbSquarePlus } from 'react-icons/tb'
 
@@ -28,17 +29,25 @@ function BottomNavItem({ href, children }: PropsWithChildren<BottomNavItemProps>
 
 export default function BottomNavigation() {
   const path = usePathname()
+  const router = useRouter()
 
+  const handleClickPlus = () => {
+    if (path === '/diary') router.push('/new/diary')
+  }
   return (
     <>
       <nav className="btm-nav btm-nav-sm border-t border-current z-navbottom box-content">
         <BottomNavItem href="/diary">
           <TbCalendar className="text-2xl" />
         </BottomNavItem>
-        <button>
-          <ModalTrigger modalId="create_note">
+        <button onClick={handleClickPlus}>
+          {path === '/note' ? (
+            <ModalTrigger modalId="create_note">
+              <TbSquarePlus className="text-2xl" />
+            </ModalTrigger>
+          ) : (
             <TbSquarePlus className="text-2xl" />
-          </ModalTrigger>
+          )}
         </button>
         <BottomNavItem href="/note">
           <TbNotebook className="text-2xl" />
